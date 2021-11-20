46aa202b6971bb687202667809c79b51ee0cb15c
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-11-20 13:18:52 +0300

    Implement Gauss sampling

--- bridges.py
+++ bridges.py
@@ -75,31 +75,91 @@ def compute_bridges_rand(adj_list: dict):
     bfs_tree_edges = list()
     visited = set()
 
-    def bfs(v):
-        visited.add(v)
-        for child in G[v]:
-            if child not in visited:
-                bfs_tree_edges.append((v, child))
-                bfs(child)
-
-    bfs(0)
-    other_edges = set([tuple(sorted(edge)) for edge in G.edges]) - set(bfs_tree_edges)
-    other_edges = list(other_edges)
-    mask_non_tree = np.random.randint(2, size=len(other_edges), dtype=np.int64)
-    mask_tree = np.empty((len(bfs_tree_edges),), dtype=np.int64)
-    # Propagate masks
-    for in_e, out_e in bfs_tree_edges[::-1]:
+    #bfs(0)
+    #other_edges = set([tuple(sorted(edge)) for edge in G.edges]) - set(bfs_tree_edges)
+    #other_edges = list(other_edges)
+    #mask_non_tree = np.random.randint(2, size=len(other_edges), dtype=np.int64)
+    #mask_tree = np.empty((len(bfs_tree_edges),), dtype=np.int64)
+    ## Propagate masks
+    #for in_e, out_e in bfs_tree_edges[::-1]:
 
 
 
 
 
-    return list(nx.algorithms.bridges(G))
+    #return list(nx.algorithms.bridges(G))
+
+
+def binary_gauss(A):
+    n = A.shape[0]
+    order = np.arange(A.shape[1])
+
+    for i in range(0, n):
+        # Search for maximum in this column
+        max_column = -1
+        for k in range(i, n):
+            if A[i][k] == 1:
+                max_column = k
+                break
+
+        # In case maximum column value is zero
+        if max_column == -1:
+            # Swap row
+            tmp = A[i, :].copy()
+            A[i, :] = A[-1, :]
+            A[-1, :] = tmp
+            # Skip iter
+            continue
+
+        # Swap maximum row with current row (column by column)
+        if max_column != i:
+            tmp = A[:, i].copy()
+            A[:, i] = A[:, max_column]
+            A[:, max_column] = tmp
+            order[i], order[max_column] = order[max_column], order[i]
+
+        # Make all rows below this one 0 in current column
+        for k in range(i + 1, n):
+            if A[k][i] != 0:
+                A[k, :] ^= A[i, :]
+
+    range_ = 0
+    for k in range(n - 1, -1, -1):
+        if A[k][k] != 0:
+            range_ = k + 1
+            break
+
+    return A, range_, order
+
+
+def sample_solutions(A, range_, order):
+    #sample = np.random.randint(-2**31, 2**31-1, size=(A.shape[0],))
+    #sample = np.random.randint(0, 2, size=(A.shape[0],))
+    sample = np.zeros(A.shape[1], dtype=np.int8)
+    free_vars = A.shape[1] - range_
+    sample[-free_vars:] = np.random.randint(0, 2, size=(free_vars,), dtype=np.uint64)
+    for i in range(range_ - 1, -1, -1):
+        #sample[i] = sample.dot(A[i, :])
+        sample[i] = np.bitwise_xor.reduce(sample & A[i, :])
+
+    return sample.take(order, axis=0)
 
 
 def compute_2bridges_rand(*args):
    pass
 
+def test():
+    a = np.array([[1, 1, 0, 0, 0, 0],
+                  [1, 0, 0, 1, 0, 1],
+                  [0, 0, 0, 1, 1, 0],
+                  [0, 0, 1, 0, 1, 1],
+                  [0, 1, 1, 0, 0, 0]], dtype=np.int8)
+    for _ in range(10):
+        res = binary_gauss(a.copy())
+        res = sample_solutions(*res)
+        print('*'* 10)
+        print(res)
+        print(a @ res % 2)
 
 if __name__ == '__main__':
-    main()
+    test()
