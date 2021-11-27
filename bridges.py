0a1676320d617c1288b81a01b41c0bb1a97e32a2
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-11-27 13:44:30 +0300

    2 bridges solved

--- bridges.py
+++ bridges.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Callable
 
 import numpy as np
 import galois
@@ -13,11 +13,6 @@ from graph_gen import (
     get_random_simple_Gnm_graph,
     get_hypercube_digraph)
 
-
-def main():
-    graph = get_random_simple_Gnm_graph(50, 50)
-    res = compute_bridges_determ(graph)
-    a = 6
 
 # детерминированный алгоритм для поиска мостов
 # на вход поступает граф представленный списком смежности
@@ -54,12 +49,6 @@ def compute_bridges_determ(adj_list):
     return bridges
 
 
-def assemble_matrix(edges: List[Tuple[int, int]], verts_len: int) -> np.array:
-    matrix = np.zeros((verts_len, len(edges)), dtype=np.int8)
-    for idx, edge in enumerate(edges):
-        matrix[edge[0]][idx] = matrix[edge[1]][idx] = 1
-    return matrix
-
 # рандомизированный алгоритм для поиска мостов
 # на вход поступает граф представленный списком смежности
 # список представлен как словарь(хеш-таблица) списков
@@ -73,6 +62,26 @@ def compute_bridges_rand(adj_list: dict):
     bridges_idxs = np.where(solutions == 0)
     bridges = np.array(edges).take(order, axis=0)[bridges_idxs].tolist()
     return {tuple(x) for x in bridges}
+
+
+def compute_2bridges_rand(adj_list: dict, argsort: Callable):
+    G = nx.Graph(adj_list)
+    edges = G.edges
+    matrix = assemble_matrix(edges, len(G)).astype(np.uint8)
+    _, rank, order = binary_gauss(matrix)
+    solutions = sample_64_bit_solutions(matrix.astype(np.uint64), rank)
+    edges_sorted = np.array(edges).take(order, axis=0)
+    sort_idxs = argsort(solutions)
+    for prev, next in zip(sort_idxs[:-1], sort_idxs[1:]):
+        if solutions[prev] == solutions[next]:
+            yield list(map(tuple, (edges_sorted[prev], edges_sorted[next])))
+
+
+def assemble_matrix(edges: List[Tuple[int, int]], verts_len: int) -> np.array:
+    matrix = np.zeros((verts_len, len(edges)), dtype=np.int8)
+    for idx, edge in enumerate(edges):
+        matrix[edge[0]][idx] = matrix[edge[1]][idx] = 1
+    return matrix
 
 
 def binary_gauss(A):
@@ -153,14 +162,12 @@ def sample_64_bit_solutions(A, rank):
 
 
 def sample_solutions(A, rank):
-    #sample = np.random.randint(-2**31, 2**31-1, size=(A.shape[0],))
-    #sample = np.random.randint(0, 2, size=(A.shape[0],))
     sample = np.zeros(A.shape[1], dtype=np.int8).view(galois.GF2)
     free_vars = A.shape[1] - rank
     if free_vars == 0:
         return sample
 
-    sample[-free_vars:] = galois.GF2.Random((free_vars,))#np.random.randint(0, 2, size=(free_vars,), dtype=np.uint64)
+    sample[-free_vars:] = galois.GF2.Random((free_vars,))
     for i in range(rank - 1, -1, -1):
         sample[i] = ((np.bitwise_xor.reduce(sample & A[i, :]) ^ A[i][i]) + np.array(1).view(galois.GF2))
 
@@ -168,90 +175,7 @@ def sample_solutions(A, rank):
     return sample
 
 
-def compute_2bridges_rand(*args):
-   pass
-
-def test_simple():
-    a = np.array([[1, 1, 0, 0, 0, 0],
-                  [1, 0, 0, 1, 0, 1],
-                  [0, 0, 0, 1, 1, 0],
-                  [0, 0, 1, 0, 1, 1],
-                  [0, 1, 1, 0, 0, 0]], dtype=np.int8)
-    a = a.view(galois.GF2)
-    for _ in range(10):
-        res = binary_gauss(a.copy())
-        res = sample_solutions(*res)
-        check = a @ res
-        if check.any():
-            print('*'* 10)
-            print(res)
-            print(check)
-
-
 def draw_solution(graph, solution):
     for edge, sol in zip(graph.edges, solution):
         graph[edge[0]][edge[1]]['tree'] = int(sol)
     print(nx.nx_pydot.to_pydot(graph))
-
-
-def test():
-    for seed in range(1000):
-        edges, nodes_count, graph = get_random_simple_Gnp_graph_edges(100, 200, seed)
-        matrix = assemble_matrix(edges, nodes_count).view(galois.GF2)
-        #print(matrix)
-        for _ in range(5):
-            res = binary_gauss(matrix.copy())
-            _, rank, order = res
-            res_to_print = np.array(res[0])
-            matrix_to_print = np.array(matrix)
-            res_to_print_ordered = res_to_print.take(order, axis=1).view(galois.GF2)
-            res = sample_solutions(*res)
-            matrix = matrix.take(order, axis=1)
-            #draw_solution(graph, res)
-            print('*'*100)
-            if not (matrix @ res).any():
-                print('OK')
-                continue
-
-            print(res)
-            print(matrix @ res)
-        a = 6
-
-
-def test_gaus_for_non_singular_matrix():
-    # Gen non singular matrix matrix
-    print('start testing')
-    iter = 1000
-    for test_i in range(iter):
-        if test_i % 10 == 0:
-            print(f'iter {test_i}')
-        n =5
-        while True:
-            #matrix = np.random.randint(0, 2, size=(n, n), dtype=np.uint64)
-            matrix = galois.GF2.Random((n, n))
-            if np.linalg.matrix_rank(matrix) == n:
-                break
-                solution = np.linalg.solve(matrix, np.zeros(n, dtype=np.uint64))
-                if solution.any():
-                    print(f'ref solution: {solution}')
-                    break
-        diag, range_, order = binary_gauss(matrix.copy())
-        # Find the roots
-        x = np.zeros(n, dtype=np.uint8).view(galois.GF2)
-        for i in range(n - 1, -1, -1):
-            x[i] = ((np.bitwise_xor.reduce(x & diag[i, :]) ^ diag[i][i]) + np.array(1).view(galois.GF2))
-        #x = x.take(order, axis=0)
-
-        if x.any():
-            print('non trivial solution')
-        res = matrix @ x
-        if res.any():
-            print('aa')
-            assert False
-
-    # Stop testing
-
-
-if __name__ == '__main__':
-    #test_gaus_for_non_singular_matrix()
-    test()
